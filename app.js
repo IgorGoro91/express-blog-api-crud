e9ec57947143189ba03791bd3c940185163f1803
IgorGoro91 <igorysc@gmail.com>
2025-02-14 12:30:41 +0100

    add:MiddleWares

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const port = 4000;
 
 const postRoutes = require('./Routers/posts');
 
+const errorsHandler = require('./middlewares/errorsHandler.js');
+const notFound = require('./middlewares/notFound.js');
+
+
+app.use( errorsHandler)
+app.use(notFound)
+
+
 app.use(express.json())
 
 app.use(express.static('public'))
